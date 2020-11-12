37c0a61f3671d8b8f708647984c0de36402d5ed9
Kiran Jagathlal <kiranjagz@gmail.com>
2020-11-12 21:09:29 +0200

    Fix list issue.

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -22,7 +22,6 @@ export class ListComponent implements OnInit, OnDestroy {
    }
 
   ngOnInit(): void {
-    this.swService.fetchCharacters();
     this.activatedRoute.params.subscribe(
       (params) => {
         this.characters = this.swService.getCharacters(params.side);
